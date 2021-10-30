685f991a5a78141f662216c19d1a33e9abdc8957
Ironmike84 <92878769+Ironmike844@users.noreply.github.com>
2021-10-30 17:17:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 pokemonList = {
     name: "bulbasaar",
     height: 7,
-    types: ['grass', 'poison']    
+    types: ['grass', 'poison'],
+    name: "Flareon",
+    height: 9,
+    types: "grass", 
 }
